22fafff3dc3dad0c48257bbf5e08fed88491b530
Dev140804 <khatriveena078@gmail.com>
2025-07-02 00:16:46 +0530

    Add dummy privacy and terms pages

---
+++ app/privacy/page.tsx
@@ -0,0 +1,1 @@
+export default function Privacy() { return <div className='p-4 text-white'>Privacy Policy placeholder.</div>; }
---
+++ app/terms/page.tsx
@@ -0,0 +1,1 @@
+export default function Terms() { return <div className='p-4 text-white'>Terms of Service placeholder.</div>; }
